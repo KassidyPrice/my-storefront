e254b2b5be2938cb9d5cac0e585549ce5183ac3d
KassidyPrice <96833138+KassidyPrice@users.noreply.github.com>
2022-12-10 22:01:21 -0700

    added About content and style

--- README.md
+++ README.md
@@ -8,16 +8,13 @@ Capstone Soft Deadline: Wed 12/14
   - Count- ask Ryan Monday
 - Cart Page
   - You should be able to add products to your cart (if there is quantity available).
-  - Checkout button =clears total when clicked?
+  - Checkout button = clears page(total and cart) when clicked?
   - remove from cart- Don't use ID, removes all of that item even if there's 2. Quantity?
 - Contact Page
   - Nicely styled contact form elements
   - This does not have to be functional, but wire up a notification that shows the user that the message was sent,
     and it should clear the form after.
-- About Page
-  - Explain your capstone
-  - Explain what you learned throughout your time here
-  - Explain your favorite languages and why
+- About Page -edit
 - Responsive designs (just mobile and desktop is fine)
 
 Keep UX in mind. Reference other sites and designs, and make this your own!
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,3 +1,41 @@
+import Header from '../parts/Header'
+import Footer from '../parts/Fotter'
+
 export default function About() {
-  return <div>About</div>
+  return (
+    <div className='about-body'>
+      <Header />
+      <p>
+        My Capstone is an E-Commerce website front end. I used React to build
+        it. I was able to practice React specific development using hooks and
+        React Router DOM. I also practiced more with Sass. I had a lot of fun,
+        and learned a lot with this project. I wasn't as intimidated by this
+        capstone as I was with the Fundementals capstone, but I was still very
+        anxious to get it finished. I'm pretty happy with how much I've been
+        able to strengthen my understanding of JS and React this semester.
+      </p>
+      <p>
+        I've learned that development is really a perfect balance between using
+        both sides of the brain. It is rational and analytical as well as
+        creative. This is what makes development so satifying, solving problems
+        as well as creating something new. I am still learning to break large
+        projects down into smaller tasks and taking it step by step. I'm always
+        surprised at how much I can accomplish if I set my anxiety aside and
+        just start with something I know how to do.
+      </p>
+      <p>
+        My favorite language really depends on the day. Some days I feel like
+        being analytical and solving functional problems. Some days I like to
+        focus on the creative styling of the page. I've really enjoyed learning
+        React becasue it combines the functioanlity and style easier and quicker
+        than the traditional HTML, JS, CSS framework. But I also like trying to
+        figure things out in vanilla JS and playing with how powerful CSS can be
+        on it's own, without the help of JS. I'm excited to learn more about the
+        backend next semester. I think I'll enjoy learning more about APIs and
+        how everthing is connected. I think I'll enjoy being able to switch
+        between working on the front or backend as projects require.
+      </p>
+      <Footer />
+    </div>
+  )
 }
